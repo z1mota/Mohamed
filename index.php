ab45323166bb9d24c9795fd9c272e73df5848d14
Mohamed Hermy <99533197+z1mota@users.noreply.github.com>
2022-05-30 06:39:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <?php
+if(isset($_GET['id'])) {
+$id = $_GET['id'];
+echo file_get_contents('https://drive.google.com/u/4/uc?id='.$id.'&export=download');
+}   else {
+    echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drive to Web Hosting</title>
+    <script src="https://bit.ly/drivetoweb2-anly"></script>
+</head>
+<body>
+    <form action="" method="get">
+     <input type="text" placeholder="Enter File ID " name="id">
+     <button type="submit">View</button>
+    </form>
+    <style>
+    input , button {
+        background : aqua ;
+        font-size : 25px ;
+        border : none ;
+        outline : none ;
+        padding : 4px ;
+    }
+    </style>
+</body>
+</html>';
 
-use App\Kernel;
+}
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+?>
+© 2022 GitHub, Inc.
+Terms
